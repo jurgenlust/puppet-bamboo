d175092e717ad519e788de2e94edd75347cb5d0c
Jurgen Lust <Jurgen.Lust@UGent.be>
2012-06-08 11:10:46 +0200

    made example more meaningful by setting all parameters

--- tests/init.pp
+++ tests/init.pp
@@ -1,16 +1,28 @@
 include tomcat
 include postgres
 
-postgres::user { 'bamboo': 
-	username => 'bamboo',
-	password => 'bamboo',
+postgres::user { 'bb_user': 
+	username => 'bb_user',
+	password => 'bamboo_secret_password',
 }
 
-postgres::db { 'bamboo':
-	name => 'bamboo',
-	owner => 'bamboo'
+postgres::db { 'bamboodb':
+	name => 'bamboodb',
+	owner => 'bb_user'
 }
 
 class { "bamboo": 
-	require => Postgres::Db['bamboo'],
+	user => "bamboo", #the system user that will own the Bamboo Tomcat instance
+	database_name => "bamboodb",
+	database_driver => "org.postgresql.Driver",
+	database_driver_jar => "postgresql-9.1-902.jdbc4.jar",
+	database_driver_source => "puppet:///modules/bamboo/db/postgresql-9.1-902.jdbc4.jar",
+	database_url => "jdbc:postgresql://localhost/bamboodb",
+	database_user => "bb_user",
+	database_pass => "bamboo_secret_password",
+	number => 5, # the Tomcat http port will be 8580
+	version => "4.1", # the Bamboo version
+	contextroot => "/",
+	webapp_base => "/opt", # Bamboo will be installed in /opt/bamboo
+	require => Postgres::Db['bamboodb'],
 }
