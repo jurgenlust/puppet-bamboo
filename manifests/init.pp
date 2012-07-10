28dc4b4ff7fb779424ede00b47b0cc713aa89a6c
Jurgen Lust <Jurgen.Lust@UGent.be>
2012-07-10 14:22:37 +0200

    Bamboo should be upgradable now

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,11 +56,19 @@ class bamboo (
 		require => Tomcat::Webapp::User[$user],
 	}
 
-	exec { "download-bamboo":
-		command => "/usr/bin/wget -O ${bamboo_dir}/tomcat/webapps/${webapp_war} ${download_url}",
+	exec { "download-bamboo" :
+		command => "/usr/bin/wget -O /tmp/${war} ${download_url}",
 		require => Tomcat::Webapp::Tomcat[$user],
-		creates => "${bamboo_dir}/tomcat/webapps/${webapp_war}",
-		timeout => 1200,	
+		creates => "/tmp/${war}",
+		timeout => 1200,
+		notify => Exec["move-bamboo"], 	
+	}
+	
+	exec { "move-bamboo":
+		command => "/bin/mv /tmp/${war} ${bamboo_dir}/tomcat/webapps/${webapp_war}",
+		require => Exec["download-bamboo"],
+		refreshonly => true,
+		notify => Tomcat::Webapp::Service[$user],
 	}
 	
 # the database driver jar
@@ -79,7 +87,7 @@ class bamboo (
 		ensure => file,
 		owner => $user,
 		group => $user,
-		require => Exec["download-bamboo"],
+		require => Exec["move-bamboo"],
 	}
 
 	tomcat::webapp { $user:
